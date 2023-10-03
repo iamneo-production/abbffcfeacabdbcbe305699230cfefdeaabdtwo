311caa764e7296aa246612bbc953cf33ad426399
Iamneo <techadmin@iamneo.ai>
2023-10-03 03:54:36 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/model/Student.java
@@ -0,0 +1,13 @@
+package com.examly.springapp.model;
+
+import lombok.Data;
+
+
+@Data
+public class Student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+
+    // Constructor, getters, and setters
+}
